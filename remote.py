ca88598fafbef37945234d304929f606e02bec57
arena <arenatemp@mailfence.com>
2023-05-09 18:10:25 +0800

    kaggle tweaks

--- remote.py
+++ remote.py
@@ -14,20 +14,22 @@ import time
 from server import Server
 
 model_folder = sys.argv[1]
+endpoint = "127.0.0.1:28888"
 
 model_storage = storage.ModelStorage(model_folder, torch.float16, torch.float32)
 params = wrapper.GenerationParameters(model_storage, torch.device("cuda"))
 
 password = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(8))
 
-server = Server(params, "127.0.0.1", "28888", password)
+ip, port = endpoint.split(':')
+server = Server(params, ip, port, password)
 server.start()
 
-print("PASSWORD: ", password)
+print("ENDPOINT:", endpoint)
+print("PASSWORD:", password)
 
 try:
     while True:
         time.sleep(1)
 except:
     server.stop()
-    #try_cloudflare.terminate(28888)
