053dc5565169402d4832440c1b4ba7229fcf54a0
arena <arenatemp@mailfence.com>
2024-04-20 13:24:44 +0800

    upscale tiling

--- upscalers.py
+++ upscalers.py
@@ -6,6 +6,9 @@ import torchvision.transforms.functional
  
 from spandrel import ImageModelDescriptor, ModelLoader
 
+TILE_SIZE = 192
+TILE_OVERLAP = 8
+
 def upscale_single(input, mode, width, height):    
     if type(input) == torch.Tensor:
         rw = width / input.shape[-1]
@@ -46,9 +49,11 @@ def upscale_super_resolution(images, model, width, height):
             while True:
                 last = image.size[0]
                 img = utils.TO_TENSOR(image).unsqueeze(0)
+                img = img[:, [2, 1, 0]] # RGB to BGR
                 img = img.to(model.device, model.dtype)
-                out = model(img)
+                out = upscale_tiled(img, model, TILE_SIZE, TILE_OVERLAP, model.scale)
                 out = out.clamp_(0, 1)
+                out = out[:, [2, 1, 0]] # BGR to RGB
                 image = utils.FROM_TENSOR(out.squeeze(0))
                 if last >= image.size[0] or (image.size[0] >= width and image.size[1] >= height):
                     break
@@ -59,6 +64,32 @@ def upscale_super_resolution(images, model, width, height):
             images[i] = upscale_single(image, transforms.InterpolationMode.LANCZOS, width, height)
     
     return images
+
+def upscale_tiled(img, model, tile_size, tile_overlap, scale):
+    device = model.device
+    b, c, h, w = img.size()
+    tile_size = min(tile_size, h, w)
+
+    if tile_size <= 0:
+        return model(img)
+
+    stride = tile_size - tile_overlap
+    h_idx_list = list(range(0, h - tile_size, stride)) + [h - tile_size]
+    w_idx_list = list(range(0, w - tile_size, stride)) + [w - tile_size]
+    result = torch.zeros(b, c, h * scale, w * scale, device=device, dtype=img.dtype)
+    weights = torch.zeros_like(result)
+    
+    for h_idx in h_idx_list:
+        for w_idx in w_idx_list:
+            in_patch = img[..., h_idx : h_idx + tile_size, w_idx : w_idx + tile_size].to(device=device)
+            out_patch = model(in_patch)
+            result[..., h_idx * scale : (h_idx + tile_size) * scale, w_idx * scale : (w_idx + tile_size) * scale,].add_(out_patch)
+            out_patch_mask = torch.ones_like(out_patch)
+            weights[..., h_idx * scale : (h_idx + tile_size) * scale, w_idx * scale : (w_idx + tile_size) * scale,].add_(out_patch_mask)
+
+    output = result.div_(weights)
+
+    return output
 
 class SR():
     @staticmethod
@@ -86,4 +117,4 @@ class SR():
             return next(self.model.model.parameters()).device
         if name == "dtype":
             return next(self.model.model.parameters()).dtype
-        return self.model.__getattr__(name)
+        return getattr(self.model, name)
